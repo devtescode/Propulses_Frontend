f2d9ae7e7b8b6835026aa6a9524844295c3c887e
agboola teslim <teslimagboola09@gmail.com>
2023-12-09 18:38:40 +0100

    first commit

--- src/components/Landingpage.jsx
+++ src/components/Landingpage.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { useNavigate } from 'react-router'
 
 const Landingpage = () => {
+    const navigate = useNavigate()
+    const LandingRegisterBTn=()=>{
+        navigate("/signup")
+    }
+    const LandingLoginBtn=()=>{
+        navigate("/login")
+    }
   return (
+    <>
     <div>Landingpage</div>
+    <button className='btn btn-success' onClick={LandingRegisterBTn}>Register</button>
+    <button className='btn btn-dark' onClick={LandingLoginBtn}>Login</button>
+    
   )
 }
 
