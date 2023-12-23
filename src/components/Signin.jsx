47f2273fcb64dfe288a7d6ca2f686c0b5a6342b8
agboola teslim <teslimagboola09@gmail.com>
2023-12-23 15:49:05 +0100

    repush

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -36,7 +36,7 @@ const Signin = () => {
         onSubmit: values => {
             setLoading(true);
             let successMessage, errorMessage;
-            axios.post("http://localhost:4500/userinvest/usersignin", { Username: values.username, Password: values.password })
+            axios.post("https://propulses.onrender.com/userinvest/usersignin", { Username: values.username, Password: values.password })
                 .then((response) => {
                     successMessage = response.data.message;
                     errorMessage = response.data.message;
