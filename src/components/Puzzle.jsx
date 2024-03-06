15ff0bb99d87a950ec0e8ccdde3bda2dfe806de4
agboola teslim <teslimagboola09@gmail.com>
2024-03-06 09:17:59 +0100

    repushsytling

--- src/components/Puzzle.jsx
+++ src/components/Puzzle.jsx
@@ -15,20 +15,20 @@ const Puzzle = () => {
             setmargin(localStorage.margin);
         }
     }, []);
-    const handleImageChange =(e)=>{
+    const handleImageChange = (e) => {
         setimgUrl(e.target.value)
-        window.history.replaceState("","", updateURLParameter(window.location.href, "img", e.target.value))
+        window.history.replaceState("", "", updateURLParameter(window.location.href, "img", e.target.value))
     }
     useEffect(() => {
-         const urlParams = new URLSearchParams(window.location.search);
-         
-         if (!urlParams.has("img")) {
-             const defaultImageUrl = "https://img.freepik.com/free-photo/jigsaw-puzzle-with-missing-piece-missing-puzzle-pieces_1150-16390.jpg?t=st=1709289735~exp=1709293335~hmac=f44208f424381c229bc6df308f71decfb0476461abb1efb9da681a8b9e664b1f&w=740"
-             setimgUrl(defaultImageUrl);
-             window.history.replaceState("", "", updateURLParameter(window.location.href, "img", defaultImageUrl));
-         } else {
-             setimgUrl(urlParams.get("img"));
-         }
+        const urlParams = new URLSearchParams(window.location.search);
+
+        if (!urlParams.has("img")) {
+            const defaultImageUrl = "https://img.freepik.com/free-photo/jigsaw-puzzle-with-missing-piece-missing-puzzle-pieces_1150-16390.jpg?t=st=1709289735~exp=1709293335~hmac=f44208f424381c229bc6df308f71decfb0476461abb1efb9da681a8b9e664b1f&w=740"
+            setimgUrl(defaultImageUrl);
+            window.history.replaceState("", "", updateURLParameter(window.location.href, "img", defaultImageUrl));
+        } else {
+            setimgUrl(urlParams.get("img"));
+        }
     }, [imgUrl]);
     // let url = "http://localhost:4500/userinvest/puzzlepage"
     // axios.post(url,{})
@@ -38,9 +38,13 @@ const Puzzle = () => {
             <Narvarbar />
             <div style={{ width: wdth + 'vw', marginLeft: margin + 'vw' }}>
                 <div className='Appp'>
-                    <h1 className=''>Sliding Puzzle</h1>
-                    <Board imgUrl={imgUrl}/>
-                    <input className='' disabled type="text" value={imgUrl} onChange={handleImageChange}/>
+                    {/* <h1 className=''>Puzzle</h1> */}
+                    <h1 className='puzzle' style={{ color: '#4F46E5', fontFamily: 'Arial, sans-serif', fontWeight: 'bold', textAlign: 'center' }}>
+                        Puzzle
+                    </h1>
+
+                    <Board imgUrl={imgUrl} />
+                    <input className='' disabled type="text" value={imgUrl} onChange={handleImageChange} />
                 </div>
             </div>
         </>
