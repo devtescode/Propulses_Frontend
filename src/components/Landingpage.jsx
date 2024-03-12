f58484485c417227ff1f322cdd1403d433f37fb4
agboola teslim <teslimagboola09@gmail.com>
2024-03-12 13:49:03 +0100

    styling repush

--- src/components/Landingpage.jsx
+++ src/components/Landingpage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router'
 import "./Landingpage.css"
+import smart from '../assets/smartphone.png'
 const Landingpage = () => {
   const navigate = useNavigate()
   const LandingRegisterBTn = () => {
@@ -51,7 +52,9 @@ const Landingpage = () => {
           <div className='landiveachone'>
             <div className='col-md-11 col-sm-11 landpadding'>
               <div className='textstylewelcome'>
-                <h1 className='fw-bold'>Welcome to Propulses where your financial aspirations take flight!</h1>
+                <h1 className='fw-bold'>Welcome to Propulses
+                  where your financial aspirations take flight!</h1>
+
               </div>
               <div className='text-center mt-3 textstyleStart'>
                 <p className='fs-5 ptagstyle'>Start your investment journey with Propulses and receive an instant $4 bonus! Plus, boost your earnings by referring friends. It's not just an investment; it's a lucrative community. Join now and watch your wealth grow!</p>
@@ -72,22 +75,95 @@ const Landingpage = () => {
 
       </div>
 
+      <div className='mx-auto text-center d-flex justify-content-center'>
+        <button class="button" data-text="Awesome">
+          <span class="actual-text">&nbsp;How Propulses Work&nbsp;</span>
+          <span aria-hidden="true" class="hover-text">How Propulses Work</span>
+        </button>
+      </div>
+
       <div class="row col-md-12 col-sm-12 mx-auto divtextlanding mt-4 mt-sm-5">
         <div class="col-sm-6 mb-3 mb-sm-0">
-          <div class="card">
+          <div class="card shadow">
             <div class="card-body">
-              <h5 class="card-title">Special title treatment</h5>
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+              <div className='d-flex' style={{ alignItems: "center" }}>
+                <div className='howitworks1'>
+                </div>
+                <div className='mx-2'>
+                  <b>
+                    Sign Up and Claim Your Instant $4 Bonus:
+                  </b>
+                </div>
+              </div>
+
+              <div className='mt-3'>
+                <p>
+                  Begin your investment journey by signing up with Propulses. As a welcome gesture, enjoy an instant $4 bonus credited to your account upon registration.
+                  This initial boost kickstarts your investment experience and sets the stage for potential earnings.</p>
+              </div>
             </div>
           </div>
         </div>
+
         <div class="col-sm-6">
           <div class="card">
             <div class="card-body">
-              <h5 class="card-title">Special title treatment</h5>
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+              <div className='d-flex' style={{ alignItems: "center" }}>
+                <div className='howitworks2'>
+                </div>
+                <div className='mx-2'>
+                  <b>
+                    Refer Friends and Boost Your Earnings:
+                  </b>
+                </div>
+              </div>
+
+              <div className='mt-3'>
+                <p>
+                  Supercharge your earnings by referring friends, family, and colleagues to Propulses. Share the wealth-building experience with your network and reap the rewards.
+                  Receive additional incentives for every successful referral, creating a community where everyone benefits from financial growth.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 mt-sm-0 mt-4">
+          <div class="card">
+            <div class="card-body">
+              <div className='d-flex' style={{ alignItems: "center" }}>
+                <div className='howitworks3'>
+                </div>
+                <div className='mx-2'>
+                  <b>
+                    Withdraw Funds Seamlessly:
+                  </b>
+                </div>
+              </div>
+
+              <div className='mt-3'>
+                <p>
+                  When the time comes to reap the rewards of your investments, our seamless withdrawal process ensures a hassle-free experience.
+                  Withdraw funds with ease, giving you the flexibility to utilize your returns or reinvest in new opportunities as you continue to build your financial future.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 mt-4">
+          <div class="card">
+            <div class="card-body">
+              <div className='d-flex' style={{ alignItems: "center" }}>
+                <div className='howitworks4'>
+                </div>
+                <div className='mx-2'>
+                  <b>
+                  Exceptional Customer Support:
+                  </b>
+                </div>
+              </div>
+
+              <div className='mt-3'>
+                <p>
+                Experience peace of mind with our dedicated customer support team. Whether you have questions, need assistance, or seek guidance, our team is readily available to ensure your journey with Propulses is smooth and enjoyable.</p>
+              </div>
             </div>
           </div>
         </div>
